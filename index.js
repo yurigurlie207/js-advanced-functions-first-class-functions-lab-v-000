3be6b2f6ec211a738e1628dc9b822caacb04eb58
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-05 15:22:19 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,7 @@ const createFareMultiplier = function (multiplyNumber) {
 const fareDoubler = createFareMultiplier(2);
 
 const fareTripler = createFareMultiplier(3);
+
+const selectDifferentDrivers = function (drivers, whichDrivers) {
+  return whichDrivers(drivers);
+};
