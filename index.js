287d324c23e337be91a92fcb0da132974c5e98a3
Adrienne Yao <adrienne.yao@icloud.com>
2021-02-05 14:41:38 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 // Code your solution in this file!
-const returnFirstTwoDrivers = function (drivers) {
-  return drivers.slice(0,2)
-}
 
 const returnFirstTwoDrivers = function (drivers) {
   return drivers.slice(0, 2);
